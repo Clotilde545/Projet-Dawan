82cb1e822a6b340f8a3fe85369dc16b9f7f3da89
Clo <clotildemangeol@gmail.com>
2019-03-27 16:46:06 +0100

    modif lien dans js astuces forum et noattaion

--- js/section2.js
+++ js/section2.js
@@ -3,12 +3,12 @@ let section2 = document.querySelector("#section2");
 //logo, volet de navigation et titre
 section2.innerHTML = `
 <article id="calculator">
-    <a href="./templates/Calculator.html" class="breadcrumb" role="button">noter votre vetement</a>
+    <a href="Calculator.html" class="breadcrumb" role="button">noter votre vetement</a>
 </article>
 <article id="astuces"> 
-    <a href="./templates/Astuces.html" class="breadcrumb" role="button">Astuces</a>
+    <a href="Astuces.html" class="breadcrumb" role="button">Astuces</a>
 </article>
 <article id="forum">
- <a href="./templates/Forum.html" class="breadcrumb" role="button">Forum</a>
+ <a href="Forum.html" class="breadcrumb" role="button">Forum</a>
 </article>
 `
