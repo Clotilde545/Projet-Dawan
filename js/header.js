7a6716c78f92b84fcbe3b5d4a23f489cda34db58
Clo <clotildemangeol@gmail.com>
2019-03-27 11:53:58 +0100

    Construction du header et header mis en flex

---
+++ js/header.js
@@ -0,0 +1,13 @@
+let header = document.querySelector("header");
+//insertion dans le header du projet 
+//logo, volet de navigation et titre
+header.innerHTML = `
+        <img src="img/logo_VS.jpg" alt="logo">
+        <nav id="nav">
+        <a href="login.html">login</a>
+        <input type="search" placeholder="Recherche" name="the_search">
+        <button type="submit">lancer la Recherche</button>
+        </nav>
+        <h1>Bienvenue sur VétiScore</h1>
+
+`
