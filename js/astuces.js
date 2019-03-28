7e66cea470aa370e4d6c85763866f1851beb250f
Clo <clotildemangeol@gmail.com>
2019-03-28 11:30:02 +0100

    Création du contenu astuces

---
+++ js/astuces.js
@@ -0,0 +1,33 @@
+let main = document.querySelector('main');
+main.innerHTML = `
+<section>
+    <article>
+        <h2>Consommer mieux</h2>
+        <h3>Consommer moins</h3>
+        <p>L'industrie de la mode produit des collections de plus en plus rapprochée et incite à la consommation.
+            Ne pas se laisser dicter la mode par l'industrie est un des éléments pour réduire sa consommation de vêtements qui polluent énormément la planète.
+        </p>
+        <h3>Louer ses vêtements</h3>
+        <p>
+            Si vous n'arrivez pas à resister aux changements de modes, une autre possibilité s'offre à vous : Louer des vêtements.
+            La location de vêtements vous permet d'avoir un changement régulier de votre garde robe sans accumuler des vêtements chez vous.
+            La location vous donne accès à des pièces de meilleures qualités que vous ne pourriez peut-être pas vous offrir en achat.
+            De plus, les vêtements seront reloués jusqu'à ce que cela ne soit plus possible.
+        </p>
+        <h3>Acheter des vêtements d'occasion</h3>
+        <p>La mode est cyclique. Du coup, dans les friperies vous pouvez trouver des pièces actuelles à prix battant toute concurrence
+            Les arrivages sont réguliers et permettent d'avoir de nouveaux choix régulièrement.
+        </p>
+    </article>
+    <article>
+        <h2>Pollution</h2>
+        <p>
+            Entre l'utilisation de substances chimiques et/ou nocives pour la production et la fabrication des fibres, le gaspillage des ressources provoqué par la surconsommation de vêtements, et la pollution générée par les kilomètres parcourus par un vêtement avant d'arriver en boutique, la mode est clairement l'une des industries les plus polluantes au monde.
+            Un t-shirt en coton peut polluer 20 000L d'eau de la fabrication à sa fin de vie.
+            Un jean pour sa fabrication utilise 2 000L d'eau
+            Seulement 1% des vêtement sont recyclés.
+            Même troué un vêtement peut avoir une seconde vie, n'hésitez pas à les recycler.
+        </p>
+    </article>
+</section>
+`
