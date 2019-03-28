79fbdead59c00dd9bc9f2dfea915f090d5c1e919
Agromy <amaucourtois@gmail.com>
2019-03-28 12:22:51 +0100

    pause midi 2803

---
+++ js/ProgressBarre.js
@@ -0,0 +1,30 @@
+'use strict'
+// création d'une fonction qui ecoute le nombre de changement sur ma classe checkbox
+let nbChange = checkbox.addEventListener("change")
+    
+console.log(count)
+
+
+let count = 3
+
+if (count === 0){
+//prend classe checkbar et met dans la classe style:"width : 0%"
+//prend classe checbar et met texte : 0/5
+}
+else if (count === 1){
+
+}
+else if (count === 2){
+    
+}
+else if (count === 3){
+    
+}
+else if (count === 4){
+    
+}
+else {
+//prend classe checkbar et met dans la classe style:"width : 100%"
+//prend classe checbar et met texte : 5/5
+// rendre bouton  Enregistrer utilisable
+}
