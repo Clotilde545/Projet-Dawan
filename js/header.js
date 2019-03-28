3cc4cd83e75ccb698fc5660c20c43f67242d9c7e
Clo <clotildemangeol@gmail.com>
2019-03-28 09:36:43 +0100

    Création de la newsletter

--- js/header.js
+++ js/header.js
@@ -4,7 +4,8 @@ let header = document.querySelector("header");
 header.innerHTML = `
         <img src="../img/logo_VS.jpg" alt="logo">
         <nav id="nav">
-        <a href="login.html">login</a>
+        <a href="./login.html">login</a>
+        <a href="./newsletter.html">Newsletter</a>
         <input type="search" placeholder="Recherche" name="the_search">
         <button type="submit">lancer la Recherche</button>
         </nav>
