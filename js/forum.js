7361a2c28131e5f91006bdcb2f7cacad5aa0ccda
Clo <clotildemangeol@gmail.com>
2019-03-29 14:27:42 +0100

    mise en forme du forum

--- js/forum.js
+++ js/forum.js
@@ -1,8 +1,10 @@
 let main = document.querySelector('main')
 main.innerHTML = `
-<h1>Forum</h1>
-<h2>Bienvenue sur le forum VétiScore</h2>
-<section>Le forum est un lieu d'échanges entre utilisateurs. Donnez-nous vos trucs et astuces</section>
+
+<h1>Bienvenue sur le forum VétiScore</h1>
+<section>
+<article>Le forum est un lieu d'échanges entre utilisateurs. Donnez-nous vos trucs et astuces</article>
+<article class = "commentaire">
 <div class="media">
     <img src="../img/profil1.jpeg" class="mr-3" alt="...">
     <div class="media-body">
@@ -10,7 +12,7 @@ main.innerHTML = `
       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
   </div>
 
-      <div class="media mt-3">
+      
         <a class="mr-3" href="#">
           <img src="../img/profil2.jpeg" class="mr-3" alt="...">
         </a>
@@ -27,6 +29,8 @@ main.innerHTML = `
     </div>
   </div>
   <button type="submit" id="newCom">Répondre</button>
+  </article>
+  </section>
 `
 
 const newCom = document.querySelector('#newCom')
