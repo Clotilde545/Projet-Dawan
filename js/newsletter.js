af8127abe18235b25802145d9aa939ef877b65db
Clo <clotildemangeol@gmail.com>
2019-03-29 16:00:55 +0100

    mise en forme newsletter

--- js/newsletter.js
+++ js/newsletter.js
@@ -10,7 +10,7 @@ newsletter.innerHTML = `
   <blockquote class="blockquote">
   <p>Le Vétiscore a été créé par 2 ingénieurs en environnement le 20 mars 2019. Depuis ils sont devenus pro du developpement informatique...ahahaha... </p>
   <p>Le site a été créé dans la foulée.</p>
-  <footer class="blockquote-footer">Someone famous (Agromy and Clo)<cite title="Source Title"></cite></footer>
+  <div class="blockquote-footer">Someone famous (Agromy and Clo)<cite title="Source Title"></cite></div>
 </blockquote>
 </article>
 
