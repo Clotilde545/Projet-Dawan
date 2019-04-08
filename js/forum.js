dbdccc8c61b2692d78fffead82db40e0151a3708
Clo <clotildemangeol@gmail.com>
2019-04-08 09:41:21 +0200

    Amélioration de l'ajout de message dans le forum

--- js/forum.js
+++ js/forum.js
@@ -6,16 +6,14 @@ main.innerHTML = `
 <article>Le forum est un lieu d'échanges entre utilisateurs. Donnez-nous vos trucs et astuces</article>
 <article class = "commentaire">
 <div class="media">
-    <img src="../img/profil1.jpeg" class="mr-3" alt="...">
+    
     <div class="media-body">
       <h5 class="mt-0">Tip top</h5>
       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
   </div>
 
       
-        <a class="mr-3" href="#">
-          <img src="../img/profil2.jpeg" class="mr-3" alt="...">
-        </a>
+        
         <div class="media-body">
           <h5 class="mt-0">C'est bien vrai =)</h5>
           Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
@@ -23,10 +21,7 @@ main.innerHTML = `
       </div>
     
 
-    <div class="media">
-    <img src="" class="mr-3" alt="">
-    <div class="media-body">
-    </div>
+    
   </div>
   <button type="submit" id="newCom">Répondre</button>
   </article>
@@ -36,6 +31,14 @@ main.innerHTML = `
 const newCom = document.querySelector('#newCom')
 
 newCom.addEventListener('click', function () {
+
+  console.log("début new div");
+
+  const futurCom = document.querySelector(".media-body:last-child")
+  const newDiv = document.createElement('div')
+    newDiv.className = 'media-body';
+    futurCom.insertAdjacentElement("afterend", newDiv);
+    console.log("fin new div");
 
   const pseudo = prompt("Entrez votre pseudo");
   console.log(`${pseudo}`);
@@ -62,13 +65,7 @@ console.log('insertion avatar');
   console.log('insertion com');
   
 
-  console.log("début new div");
 
-  const futurCom = document.querySelector(".media-body:last-child")
-  const newDiv = document.createElement('div')
-    newDiv.className = 'media-body';
-    futurCom.insertAdjacentElement("afterend", newDiv);
-    console.log("fin new div");
 
 
 });
